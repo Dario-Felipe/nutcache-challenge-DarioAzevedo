52b29507dc7ccca45781646a21b3eb94282b40e7
Dario-Felipe <dariofelipe91@gmail.com>
2021-03-06 19:24:13 -0300

    refactor: adjust components path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import GlobalStyle from './theme/globalStyle';
 import Header from './components/Header/index';
-import EmployeeList from './components/EmployeeList';
-import FixedCreate from './components/FixedCreate';
+import EmployeeList from './components/EmployeeList/index';
+import FixedCreate from './components/FixedCreate/index';
 
 function App() {
   return (
